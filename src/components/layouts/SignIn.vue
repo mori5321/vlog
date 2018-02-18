9ec0c7aef82ec563b5ef298643389da603a1ff60
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-02-18 23:06:01 +0900

    login authentication

--- src/components/layouts/SignIn.vue
+++ src/components/layouts/SignIn.vue
@@ -56,6 +56,7 @@ export default {
         })
         this.$store.dispatch('setToken', response.data.token)
         this.$store.dispatch('setUser', response.data.user)
+        this.$router.push({ path: "/" })
       } catch(error) {
         console.log(error)
         this.error = error
--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,36 @@ import List from '@/components/layouts/List'
 import Feed from '@/components/layouts/Feed'
 import SignIn from '@/components/layouts/SignIn'
 import Edit from '@/components/Markdown/Edit'
+import Store from '@/store/store.js'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     { path: '/', name: 'List', component: List },
-    { path: '/markdown', name: 'MarkdownEditor', component: MarkdownEditor },
+    { path: '/markdown', name: 'MarkdownEditor', component: MarkdownEditor,
+      meta: {
+        requiresAuth: true
+      }
+    },
     { path: '/feed/:id', name: 'Feed', component: Feed },
     { path: '/sign_in', name: 'SignIn', component: SignIn },
-    { path: '/feed/:id/edit', name: 'Edit', component: Edit }
+    { path: '/feed/:id/edit', name: 'Edit', component: Edit,
+      meta: {
+        requiresAuth: true
+      }
+    }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth) && !Store.state.isUserLoggedIn) {
+    next({
+      path: '/sign_in'
+    })
+  } else {
+    next()
+  }
+})
+
+export default router
